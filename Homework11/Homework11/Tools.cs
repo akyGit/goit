75ffb62a79bf2af3b346b4d37d7d5de9d8a194ab
Viktor Martyniuk <viktor.mart@gmail.com>
2016-08-21 20:23:59 +0300

    added headers to file

--- Homework11/Homework11/Tools.cs
+++ Homework11/Homework11/Tools.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Author: Viktor Matyniuk
+ * Date: 21 August, 2016
+ * Description: homework 11 (goit)
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
