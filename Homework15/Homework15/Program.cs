dba1ab6251c528508ce5f15268d8f3ee26b86245
Viktor Martyniuk <viktor.mart@gmail.com>
2016-09-30 23:48:10 +0300

    added homework15

---
+++ Homework15/Homework15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework15 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("I started, but I don't have a time.\n" + 
+                "My mother is arriving to me tomorrow. This will be first time in eight years.\n" + 
+                "Thank you. Have a nice time of day");
+        }
+    }
+}
